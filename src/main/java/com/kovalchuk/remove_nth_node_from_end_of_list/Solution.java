875262290797c8b90ae8731c2092170b1e9c5f1e
Aleksey Kovalchuk <aleksey0koval@gmail.com>
2022-11-29 23:17:55 +0100

    feat: add solution for task 'Remove Nth Node From End of List'

---
+++ src/main/java/com/kovalchuk/remove_nth_node_from_end_of_list/Solution.java
@@ -0,0 +1,53 @@
+package com.kovalchuk.remove_nth_node_from_end_of_list;
+
+import java.util.Objects;
+
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode resultNode = new ListNode(0);
+        ListNode slow = resultNode;
+        ListNode fast = resultNode;
+        slow.next = head;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+
+        return resultNode.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val && Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
+    }
+}
